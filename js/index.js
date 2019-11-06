4ebb940aacfd840dfe2e574b2f4f757570b57b9c
hbz12330 <34837798+hbz12330@users.noreply.github.com>
2019-11-06 16:14:42 +0800

    更改
    新增内容

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('#ex1').slider({
+    formatter: function(value) {
+        return 'Current value: ' + value;
+    }
+});
+
+// Without JQuery
+var slider = new Slider('#ex1', {
+    formatter: function(value) {
+        return 'Current value: ' + value;
+    }
+});
